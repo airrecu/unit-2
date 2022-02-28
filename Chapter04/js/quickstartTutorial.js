f05d4bb52e5184c9f048c24647546a2e38236046
airrecu <65466712+airrecu@users.noreply.github.com>
2022-02-28 01:40:29 -0600

    Prior to Attempting 2.5
    Working Map with Orange Dots of MegaCities before 2.5

--- Chapter04/js/quickstartTutorial.js
+++ Chapter04/js/quickstartTutorial.js
@@ -1,8 +1,19 @@
 // Add all scripts to the JS folder
 
+
+
+// creates a variable named map, 
+// map() associates it to the "map" div
+// setView() assigns to it an object wtih properites for center and zoom
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 
+
+
+
+
+// tilelayer() function loads and displays a tilelayer with options set for zoom and other features
+// addTo()  displays the tilelayer in the map div
 
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -12,10 +23,10 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{e
 	ext: 'jpg'
 }).addTo(map);
 
-
+// marker() creates icons that clickable or draggable
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
-
+// circle() instantiates a class for drawing a circle overlay
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -24,18 +35,22 @@ var circle = L.circle([51.508, -0.11], {
 }).addTo(map);
 
 
+
+// polygon() instantiates a class for drawing a polygon overlay
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
 
-
+// bindPopup() gives the object a popup message given as the parameter
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.")
 
 
+// popup() gives the object a popup message given as the parameter
+// use with the onMapClick() to respond to click events on map
 var popup = L.popup();
 
 function onMapClick(e) {
